8245edba34c078444124c201f41210fa6b3ad134
megfly <mflynn333@gmail.com>
2020-03-31 17:40:09 -0700

    edit application controller and users controller; delete welcome.erb; re-saved seeds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,26 @@ class UsersController < ApplicationController
     get '/login' do 
         erb :login
     end 
+
     #receive the login form, find the user, log the user in
     post '/login' do 
+        #find user, authenticate user, log in user, redirect to users landing page
+        @user = User.find_by(username: params[:username])
+            if @user.authenticate(params[:password])
+                session[:user_id] = @user.id
+                redirect "users/#{@user.id}"
+            else 
+        end
     end 
 
     #signup
     get '/signup' do 
+
+    end 
+
+    #user show route 
+    get '/users/:id' do 
+        "this will be the user show route"
     end 
 
 end 
