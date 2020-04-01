b2945d7d1ae61bae692bd7849559cd5805c1806e
megfly <mflynn333@gmail.com>
2020-04-01 14:47:40 -0700

    make a aerial entries controller and a aerial entries folder with a new entries file, a new index file and a new show file

---
+++ app/controllers/aerial_entries_controller.rb
@@ -0,0 +1,13 @@
+class AerialEntriesController < ApplicationController
+
+    #get to render form
+    get '/aerial_entries/new' do 
+        erb :'/aerial_entries/new'
+    end 
+
+    #post aerial entries to create a new entry
+
+    #show route
+
+    #index for all
+end 
