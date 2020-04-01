dd8ff50d54b5c095ec3196de7ad517532aa8ba6f
megfly <mflynn333@gmail.com>
2020-04-01 16:06:25 -0700

    add post aerial entries to controller and modify new.erb form

--- app/controllers/aerial_entries_controller.rb
+++ app/controllers/aerial_entries_controller.rb
@@ -6,6 +6,20 @@ class AerialEntriesController < ApplicationController
     end 
 
     #post aerial entries to create a new entry
+    post '/aerial_entries' do 
+        #create new entry and save to db. Only want to save entry if it has content. Only create if user is logged in.
+        if !logged_in?
+            redirect '/'
+        end 
+        if params[:move_name] !="" && [:apparatus] !="" && [:difficulty] !="" && [:description] !="" #is not an empty string
+            #create new
+            @aerial_entry = AerialEntry.create(move_name: params[:move_name], apparatus: params[:apparatus], difficulty:
+            params[:difficulty], description: params[:description], user_id: current_user.id)
+            redirect "/aerial_entries/#{@aerial_entry.id}"
+        else 
+            redirect '/aerial_entries/new'
+        end 
+    end 
 
     #show route
 
