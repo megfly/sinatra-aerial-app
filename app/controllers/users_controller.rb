c03fbb8279d9404459159c86027500d9f77347a5
megfly <mflynn333@gmail.com>
2020-04-03 15:35:23 -0700

    add some more flash messages and errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
                 puts session 
                 redirect "users/#{@user.id}"
             else 
-                flash[:message] = "Invalid username or password. Please sign up or try again."
+                flash[:error] = "Invalid username or password. Please sign up or try again."
                 redirect '/login'
         end
     end 
@@ -29,10 +29,12 @@ class UsersController < ApplicationController
         if params[:name] != "" &&[:email] !="" && params[:password] != "" #not equal to string
             @user = User.create(params)
             session[:user_id] = @user.id #logging user in 
+            flash[:message] = "Your account has been created, #{@user.name}!"
             redirect "/users/#{@user.id}" #url
         else 
             #not valid input
             #maybe we should include a message to the user telling them what is wrong
+            flash[:error] = "Account creation failed. Please enter a username and password."
             redirect '/signup'
         end 
     end 
