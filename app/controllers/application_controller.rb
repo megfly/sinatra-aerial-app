d0de0911514c12b781092c257f23258fc05a481c
megfly <mflynn333@gmail.com>
2020-04-03 12:04:00 -0700

    register Sinatra Flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "aerial_moves_app"
+    register Sinatra::Flash 
   end
 
   get "/" do
