4edeb4a5214b1a70af6efa15f5f58415da2f4ea2
megfly <mflynn333@gmail.com>
2020-04-05 17:24:51 -0700

    cleaning up routes and trying to figure out why some arent working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,76 @@ class ApplicationController < Sinatra::Base
     end 
   end
 
+#signup - render the signup form
+  get '/signup' do 
+    erb :signup
+  end 
+
+    post '/users' do #create new user and persist new user to the database
+        #{"name"=>"Name", etc}
+      if params[:name] != "" && [:username] !="" && params[:password] != "" #not equal to  empty string
+            @user = User.new(name: params["name"], username: params["username"], password: params["password"])
+            @user.save
+
+            session[:user_id] = @user.id #logging user in 
+            flash[:message] = "Your account has been created, #{@user.name}!"
+            # THIS ISNT WORKING
+            redirect "/users/show"  #{@user.id}" #url
+        else 
+            #not valid
+            flash[:error] = "Account creation failed. Please enter a name, username and password."
+            redirect '/signup'
+        end 
+    end 
+
+
+  #login - render the login form
+  get '/login' do 
+    erb :login
+  end 
+
+  #receive the login form, find the user, log the user in
+  post '/login' do 
+    #find user, authenticate user, log in user, redirect to users landing page
+    @user = User.find_by(username: params[:username])
+      if @user.authenticate(params[:password]) #if user returns nil (falsey) it would go to else .....#if @user.save && authenticate??
+        session[:user_id] = @user.id
+        puts session 
+        redirect "users/#{@user.id}"
+      else 
+        flash[:error] = "Invalid username or password. Please sign up or try again."
+        redirect '/login'
+      end
+    end 
+
+    
+    get '/users/show' do 
+        @user = User.find(session[:user_id])
+        erb :'/users/show'
+    end 
+
+
+
+        #user show route 
+        get '/users/:id' do 
+          if !logged_in? #if not logged in
+              flash[:error] = "Must be logged in to view this page"
+              redirect '/users/login'
+          else
+              @user = User.find_by(id: params[:id])
+      #THE BELOW ERB ISNT WORKING
+              erb :'/users/show'
+          end 
+      end 
+
+  get '/logout' do 
+    session.clear
+    redirect '/'
+  end 
+
+
+
+
 
   helpers do 
 
