fe510acfe1d31e8ee03374421e8f3d229958dc51
megfly <mflynn333@gmail.com>
2020-04-14 17:51:16 -0700

    edited post /users route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,9 @@ class UsersController < ApplicationController
     
     post '/users' do #create new user and persist new user to the database
         if params[:name] != "" && [:username] !="" && params[:password] != "" #not equal to  empty string
-          @user = User.new(name: params["name"], username: params["username"], password: params["password"])
-          @user.save
-    
+
+          @user = User.create(name: params["name"], username: params["username"], password: params["password"])
+          #@user.save
           session[:user_id] = @user.id #logging user in 
           flash[:message] = "Your account has been created, #{@user.name}!"
           redirect "users/#{@user.id}"  #{@user.id}" #url
@@ -29,18 +29,4 @@ class UsersController < ApplicationController
         end 
     end 
   
-    # helpers do 
-
-    #     def logged_in? 
-    #       !!current_user 
-    #       #double bang takes value and turns it into boolean, true if user logged in, otherwise false (context: true, and then negated(false) and then negated again (true))
-    #     end 
-    
-    #     def current_user 
-    #       @current_user ||= User.find_by(id: session[:user_id])
-    #       #This Operator only sets the variable if the variable is false or Nil. so x ||= y this means x || x = y so if x is nil or false set x to be the value of y.
-    #     end 
-    
-    # end 
-
 end 
