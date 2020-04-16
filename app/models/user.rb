8c8b4cc11a7adad069a67f1c4bca03f8a83af8d0
megfly <mflynn333@gmail.com>
2020-04-15 17:08:05 -0700

    add validates :name, presence: true

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base 
     has_secure_password
+    
     has_many :aerial_entries
 
+    validates :name, presence: true 
     validates :username, presence: true 
     validates :username, uniqueness: true 
 end 
