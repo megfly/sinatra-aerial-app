36f33f7f4d3b58b49293fd901372de5295e3fd37
megfly <mflynn333@gmail.com>
2020-04-15 17:07:36 -0700

    redid the post /users route because it wasnt working with the ActiveRecord validations in the User.rb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,31 @@ class UsersController < ApplicationController
         erb :signup
     end 
     
-    post '/users' do #create new user and persist new user to the database
-        if params[:name] != "" && [:username] !="" && params[:password] != "" #not equal to  empty string
+    # post '/users' do #create new user and persist new user to the database
+    #     if params[:name] != "" && [:username] !="" && params[:password] != "" #not equal to  empty string
 
-          @user = User.create(name: params["name"], username: params["username"], password: params["password"])
-          #@user.save
-          session[:user_id] = @user.id #logging user in 
+    #       @user = User.create(name: params["name"], username: params["username"], password: params["password"])
+    #       #@user.save
+    #       session[:user_id] = @user.id #logging user in 
+    #       flash[:message] = "Your account has been created, #{@user.name}!"
+    #       redirect "users/#{@user.id}"  #{@user.id}" #url
+    #     else 
+    #       flash[:error] = "All fields required."
+    #       redirect '/signup'
+    #     end 
+    #   end 
+
+    post '/users' do 
+      @user = User.new(params)
+        if @user.save #we are using ActiveRecord validations in User.rb, AR validations prevents creation, saving or updating in db if requirement not met
+          session[:user_id] = @user.id 
           flash[:message] = "Your account has been created, #{@user.name}!"
-          redirect "users/#{@user.id}"  #{@user.id}" #url
+          redirect "users/#{@user.id}"
         else 
           flash[:error] = "All fields required."
           redirect '/signup'
         end 
-      end 
+    end 
 
     get '/users/:id' do #user show route- dynamic
         if !logged_in? #if not logged in
